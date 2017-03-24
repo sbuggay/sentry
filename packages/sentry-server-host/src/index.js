9a61b8b24a89a7078e7b1855804a2717437bf5a3
Devan Buggay <debuggay@microsoft.com>
2017-03-24 16:18:00 -0700

    Reset static use

--- packages/sentry-server-host/src/index.js
+++ packages/sentry-server-host/src/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, "../node_modules/sentry-client-dist/dist")));
+app.use(express.static(path.join(__dirname, "../node_modules/sentry-client-dist/")));
 
 // Listen for requests
-var server = app.listen(3000, function () {
-
+var server = app.listen(port = 3333, () => {
+	console.log(`listening on port ${port}`);
 });
