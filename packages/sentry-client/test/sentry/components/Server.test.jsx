4012cffa0ed200857fe0448da429072598631941
Jack Ford <jford@salsify.com>
2017-03-28 20:10:14 -0400

    Its a start

--- packages/sentry-client/test/sentry/components/Server.test.jsx
+++ packages/sentry-client/test/sentry/components/Server.test.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { Server } from "../../../src/sentry/components/Server";
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Server from '../../../src/sentry/components/Server';
 
-test("test Server component", () => {
-	const component = renderer.create(
-		<Server hostname="TEST_HOSTNAME" />
-	);
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
-	let tree = component.toJSON();
-	expect(tree).toMatchSnapshot();
+import reducer from '../../../src/sentry/reducer';
+import { removeServer } from '../../../src/sentry/actions';
+import { createStore } from 'redux';
+
+test('Server', () => {
+	const mockStore = configureStore();
+	const initialState = {
+		app: {
+			servers: [{}, {}],
+			index: 1
+		}
+	};
+
+	let store, container;
+
+	beforeEach(()=>{
+		store = mockStore(initialState);
+		container = shallow(<ConnectedServer store={store} />);
+	})
+
+	it('has a button to remove a server', () => {
+		expect(component.contains(
+			<div>
+				<div className='fa fa-times'></div>
+			</div>
+		)).toBe(true);
+	});
+
+	it('checks removeServer on dispatch', () => {
+		store.dispatch(removeServer(initialState.index));
+		const action = store.getActions();
+
+		expect(action[0].type).toBe('REMOVE_SERVER');
+	});
 });
