408a3d7cb063f4935940f9d562b4089803f19334
sbuggay <sbuggay@gmail.com>
2017-06-30 11:06:17 -0700

    Factored out formatBytes

--- packages/sentry-client/src/sentry/lib/utils.ts
+++ packages/sentry-client/src/sentry/lib/utils.ts
@@ -8,3 +8,10 @@ export function guid() {
     return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
         s4() + "-" + s4() + s4() + s4();
 }
+
+export function formatBytes(bytes: number): string {
+    if (bytes < 1024) return bytes + "B";
+    else if (bytes < 1048576) return (bytes / 1024).toFixed(1) + "KB";
+    else if (bytes < 1073741824) return (bytes / 1048576).toFixed(1) + "MB";
+    else return (bytes / 1073741824).toFixed(1) + "GB";
+};
