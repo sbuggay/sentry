45317dd97a05f8d14429acf553a6122b71e3e838
sbuggay <sbuggay@gmail.com>
2017-07-10 22:09:11 -0700

    Add pretty time

---
+++ packages/sentry-client/src/sentry/utils/prettyTime.ts
@@ -0,0 +1,42 @@
+export function pretty(seconds: number, scale: number = 1): string {
+
+    // Windows returns fractions of seconds, we don't care about that
+    seconds = Math.floor(seconds);
+
+    let minutes = (seconds / 60) | 0;
+    seconds -= minutes * 60;
+
+    let hours = (minutes / 60) | 0;
+    minutes -= hours * 60;
+
+    let days = (hours / 24) | 0;
+    hours -= days * 24;
+
+    const weeks = (days / 7) | 0;
+    days -= weeks * 7;
+
+    const times = [weeks, days, hours, minutes, seconds];
+
+    const types = [
+        "week",
+        "day",
+        "hour",
+        "minute",
+        "second"
+    ];
+
+    function getTimeString(value: number, index: number) {
+        return `${value} ${types[index]}${value === 1 ? "" : "s"}`;
+    }
+
+    const str: string[] = [];
+    times.forEach((time: number, index: number) => {
+        if (time !== 0) {
+            str.push(getTimeString(time, index));
+        }
+    });
+
+    str.splice(scale);
+
+    return str.join(" ");
+}
