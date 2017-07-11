da27f4a573b414294da5a6ec59b6ed8940fd83e5
sbuggay <sbuggay@gmail.com>
2017-07-10 22:08:13 -0700

    Add progress bar component

---
+++ packages/sentry-client/src/sentry/components/Bar.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+
+interface IBarProps {
+    percentage: number;
+    text?: string;
+    style?: React.CSSProperties;
+    barStyle?: React.CSSProperties;
+}
+
+export class Bar extends React.Component<IBarProps, any> {
+
+    public getStyle(): React.CSSProperties {
+        return {
+            width: "100%",
+            height: "16px",
+            boxSizing: "border-box",
+            margin: "2px",
+            border: "1px solid gray",
+            ...this.props.style
+        };
+    }
+
+    public getBarStyle(): React.CSSProperties {
+        return {
+            width: `${this.props.percentage}%`,
+            height: "100%",
+            background: "#62C552",
+            ...this.props.barStyle
+        };
+    }
+
+    public getTextStyle(): React.CSSProperties {
+        return {
+            position: "absolute",
+            fontSize: "12px"
+        };
+    }
+
+    public render(): JSX.Element | null {
+        return (
+            <div style={this.getStyle()}>
+                <div style={this.getTextStyle()}>
+                    {this.props.text ? this.props.text : null}
+                </div>
+                <div style={this.getBarStyle()}>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Bar;
