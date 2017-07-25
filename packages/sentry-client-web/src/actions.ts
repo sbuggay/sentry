06af45b809b64fae28e798aba16fd1a951cdcdcd
sbuggay <sbuggay@gmail.com>
2017-07-25 10:51:50 -0700

    Replaced fetch

--- packages/sentry-client-web/src/actions.ts
+++ packages/sentry-client-web/src/actions.ts
@@ -49,70 +49,35 @@ export const initializePolling = () => {
 
 export const pollServers = () => {
     return (dispatch: Dispatch<IState>, getState: () => IState) => {
-        const endpoint = "http://127.0.0.1:3030/api";
-        const requestInit: RequestInit = {
-            method: "GET",
-            headers: new Headers(),
-            cache: "no-store"
-        };
+        const endpoint = "http://localhost:3030/api";
 
         dispatch({
             type: actionTypes.SERVER_POLL
         });
 
-        fetch(endpoint, requestInit).then((response: Response) => {
-            response.json().then((data: JSON) => {
-                console.log(data);
-                dispatch({
-                    type: actionTypes.SERVER_POLL_SUCCESS,
-                    payload: data
-                });
-            });
-        }).catch((error: Error) => {
-            dispatch({
-                type: actionTypes.SERVER_POLL_FAILURE,
-                payload: error
-            });
-        });
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (this.readyState === this.DONE) {
+                if (this.status === 200) {
+                    const data = JSON.parse(this.responseText);
+                    dispatch({
+                        type: actionTypes.SERVER_POLL_SUCCESS,
+                        payload: data
+                    });
+                } else {
+                    dispatch({
+                        type: actionTypes.SERVER_POLL_FAILURE,
+                        payload: this.responseText
+                    });
+                }
+            }
+        };
+
+        xhr.open("GET", endpoint, true);
+        xhr.send();
     };
 };
-
-// export const pollServer = (server: any) => {
-//     return (dispatch: Dispatch<IState>, getState: () => IState) => {
-//         const requestInit: RequestInit = {
-//             method: "GET",
-//             headers: new Headers(),
-//             cache: "no-store"
-//         };
-
-//         fetch(server.host, requestInit).then((response: Response) => {
-//             // We care about the responseCode and the body
-
-//             // Go ahead and set status to unavailable
-//             const status = response.ok ? EStatus.available : EStatus.outage;
-//             response.json().then((data: JSON) => {
-//                 dispatch({
-//                     type: actionTypes.SERVER_POLL_SUCCESS,
-//                     payload: {
-//                         id: server.id,
-//                         status,
-//                         ...data
-//                     }
-//                 });
-//             });
-
-//             dispatch(updateLastUpdated(Date.now()));
-//         }).catch((reason: Error) => {
-//             dispatch({
-//                 type: actionTypes.SERVER_POLL_FAILURE,
-//                 payload: {
-//                     id: server.id,
-//                     status: EStatus.outage,
-//                 }
-//             });
-//         });
-//     };
-// };
 
 export const changeView = (view: number) => {
     return {
