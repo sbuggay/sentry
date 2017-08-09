d9729c352d232b526815aceeed004a162b77bbc2
sbuggay <sbuggay@gmail.com>
2017-08-08 18:42:34 -0700

    Merge

--- packages/sentry-server-host/src/api.ts
+++ packages/sentry-server-host/src/api.ts
@@ -6,20 +6,10 @@ import { pollServers } from "./polling";
 import { configGet } from "./configHandler";
 
 router.get("/", (req, res) => {
-<<<<<<< HEAD
     const hosts = configGet("servers").map((server: any) => server.host);  
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Content-Type: application/json");
-=======
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Content-Type: application/json");
-    let hosts = [
-        "http://127.0.0.1:3333",
-        "http://127.0.0.1:3333",
-        "http://127.0.0.1:3333"
-    ];
->>>>>>> 66d896b3ce62ff1b5b156f6d0694db3a9ae36077
     pollServers(hosts).then((result) => {
         res.send(result);
     });
