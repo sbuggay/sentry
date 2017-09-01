6a3e1ef6ea9822bbb059faef5031ac1649184c7d
Jack Ford <jford8820@gmail.com>
2017-09-01 15:15:38 -0400

    Initial commit

--- packages/sentry-client-web/test/components/Server.test.tsx
+++ packages/sentry-client-web/test/components/Server.test.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
+import { shallow, mount } from "enzyme";
 import * as renderer from "react-test-renderer";
-import { Server } from "../../src/components/Server";
+import ConnectedServer, { Server } from "../../src/components/Server";
+import sinon from "sinon";
+
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+import reducer from "../../src/reducer";
+import { removeServer } from "../../src/actions";
+import { createStore } from "redux";
 
 describe("Server", () => {
 	it("renders server", () => {
@@ -22,65 +31,51 @@ describe("Server", () => {
 
 });
 
-// import * as React from "react";
-// import { shallow, mount } from "enzyme";
-// import * as renderer from "react-test-renderer";
-// import ConnectedServer, { Server } from "../../../src/sentry/components/Server";
+describe("ConnectedServer", () => {
+	const mockStore = configureStore();
+	const initialState = {
+		index: 0,
+		app: {
+			servers: [{}, {}]
+		}
+	};
 
-// import configureStore from "redux-mock-store";
-// import { Provider } from "react-redux";
+	let store: any, wrapper: any;
 
-// import reducer from "../../../src/sentry/reducer";
-// import { removeServer } from "../../../src/sentry/actions";
-// import { createStore } from "redux";
+	beforeEach(() => {
+		store = mockStore(initialState);
+		wrapper = shallow(<ConnectedServer store={store} />);
+	});
 
-// xdescribe("ConnectedServer", () => {
-// 	const mockStore = configureStore();
-// 	const initialState = {
-// 		index: 0,
-// 		app: {
-// 			servers: [{}, {}]
-// 		}
-// 	};
+	it("has a button to remove a server", () => {
+		expect(wrapper.contains(
+			<div>
+				<div className="fa fa-times"></div>
+			</div>
+		)).toBe(true);
+	});
 
-// 	let store: any, wrapper: any;
+	it("triggers the action on click", () => {
+		const server = {
+			name: "",
+			host: "",
+			status: 0,
+			data: {
+			}
+		};
+		const onClick = sinon.spy();
+		wrapper = mount(
+			<ConnectedServer data={server} index={0} onClick={onClick} />
+		);
 
-// 	beforeEach(() => {
-// 		store = mockStore(initialState);
-// 		wrapper = shallow(<ConnectedServer store={store} />);
-// 	});
+		wrapper.find(".fa-times").simulate("click");
+		expect(onClick.calledOnce).to.equal(true);
+	});
 
-// 	it("has a button to remove a server", () => {
-// 		expect(wrapper.contains(
-// 			<div>
-// 				<div className="fa fa-times"></div>
-// 			</div>
-// 		)).toBe(true);
-// 	});
+	it("checks removeServer on dispatch", () => {
+		store.dispatch(removeServer(initialState.index));
+		const action = store.getActions();
 
-// 	it("triggers the action on click", () => {
-// 		const onClick = sinon.spy();
-// 		wrapper = mount(
-// 			<ConnectedServer onClick={onClick} />
-// 		);
-
-// 		wrapper.find(".fa-times").simulate("click");
-// 		expect(onButtonClick.calledOnce).to.equal(true);
-// 	});
-
-// 	it("checks removeServer on dispatch", () => {
-// 		store.dispatch(removeServer(initialState.index));
-// 		const action = store.getActions();
-
-// 		expect(action[0].type).toBe("REMOVE_SERVER");
-// 	});
-// });
-
-// xtest("Server", () => {
-// 	const component = renderer.create(
-// 		<Server hostname="TEST_HOSTNAME" />
-// 	);
-
-// 	let tree = component.toJSON();
-// 	expect(tree).toMatchSnapshot();
-// });
+		expect(action[0].type).toBe("REMOVE_SERVER");
+	});
+});
