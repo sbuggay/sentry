b641938d4a81184ab3eb20c5aec8451575d00318
Devan Buggay <sbuggay@gmail.com>
2018-08-05 21:03:36 -0700

    Sentry client web/ux cleanup (#14)
    * UX Cleanup
    
    * Publish
    
     - sentry-client-web@0.2.2
     - sentry-server-data@0.3.2
     - sentry-server-host@0.2.2
    
    * Update snap
    
    * Fix packages

--- packages/sentry-client-web/src/actions.ts
+++ packages/sentry-client-web/src/actions.ts
@@ -74,6 +74,7 @@ export const pollServers = () => {
                         payload: this.responseText
                     });
                 }
+                dispatch(updateLastUpdated(Date.now()));
             }
         };
 
